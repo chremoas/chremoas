cac60a8e3f9e184e1d41f35ef73c5ad604951baa
Brian Hechinger <wonko@4amlunch.net>
2021-03-17 16:16:21 -0400

    So many things to check in that I've ignored for so long. I'm a terrible person.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/chremoas/chremoas
 go 1.14
 
 require (
-	github.com/chremoas/services-common v1.3.1
+	github.com/chremoas/services-common v1.3.2
 	github.com/golang/protobuf v1.3.2
 	github.com/lusis/go-slackbot v0.0.0-20180109053408-401027ccfef5 // indirect
 	github.com/lusis/slack-test v0.0.0-20190426140909-c40012f20018 // indirect
@@ -11,8 +11,10 @@ require (
 	github.com/micro/go-bot v1.1.0
 	github.com/micro/go-micro v1.9.1
 	github.com/micro/micro v1.8.0
+	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80
 )
 
 replace github.com/chremoas/chremoas => ../chremoas
+
 replace github.com/hashicorp/consul => github.com/hashicorp/consul v1.5.1
--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/chremoas/chremoas/bot"
+
 	"github.com/micro/go-bot/input"
 	_ "github.com/micro/go-bot/input/discord"
 	_ "github.com/micro/go-bot/input/hipchat"
 	_ "github.com/micro/go-bot/input/slack"
 	"github.com/micro/go-micro/config/cmd"
+	"go.uber.org/zap"
+
+	chremoasPrometheus "github.com/chremoas/services-common/prometheus"
+
+	"github.com/chremoas/chremoas/bot"
 )
 
-var Version = "SET ME YOU KNOB"
+var (
+	Version = "SET ME YOU KNOB"
+	logger  *zap.Logger
+)
 
 func main() {
+	var err error
+
+	// TODO pick stuff up from the config
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	logger.Info("Initialized logger")
+
+	go chremoasPrometheus.PrometheusExporter(logger)
+
 	app := cmd.App()
 	app.Flags = bot.DefaultFlags
 	//app.Commands = append(app.Commands, bot.Commands()...)
@@ -37,7 +57,7 @@ func main() {
 
 	bot.App = app
 
-	err := cmd.Init(
+	err = cmd.Init(
 		cmd.Name("chremoas"),
 		cmd.Description("A bot to kill the Dramiel"),
 		cmd.Version(Version),
